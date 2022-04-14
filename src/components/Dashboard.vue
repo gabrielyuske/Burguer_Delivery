8aedbc9eabbf5cc2555311f7195bea24219fd01b
gabrielyuskesato <gabrielyuskesato@icloud.com>
2022-04-14 21:15:48 +0900

    requisicaes com vue.js e json finalizado

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="burger-table">
+      <Message :msg="msg" v-show="msg" />
       <div>
           <div id="burger-table-heading">
               <div id="order-id">#:</div>
@@ -7,7 +8,8 @@
               <div>Pao:</div>
               <div>Carne:</div>
               <div>Opcionais:</div>
-              <div>Acoes:</div>
+              <!-- <div>Acoes:</div> -->
+              
           </div>
       </div>
       <div id="burger-table-rows">
@@ -18,15 +20,16 @@
               <div>{{ burger.carne }}</div>
               <div>
                 <ul>
-                  <li>salame</li>
-                  <li>tomate</li>
+                  <li v-for="(opcional,index) in burger.opcionais" :key="index">{{opcional}}</li>
+                  
                 </ul>
               </div>
               <div>
-                  <select name="status" class="status">
-                      <option value="">Status</option>
+                  <select name="status" class="status" @change="updateBurger($event, burger.id)">
+                      <option value="">Selecione</option>
+                      <option v-for="s in status" :key="s.id" :value="s.tipo" :selected="burger.status == s.tipo">{{s.tipo}}</option>
                   </select>
-                  <button class="delete-btn">Cancelar</button>
+                  <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
               </div>           
           </div>
       </div>
@@ -34,13 +37,18 @@
 </template>
 
 <script>
+import Message from "./Message.vue"
 export default {
     name: "Dashboard",
+    components:{
+        Message
+    },
     data(){
         return{
             burgers: null,
             burger_id: null,
-            status: []
+            status: [],
+            msg: null
         }
     },
     methods:{
@@ -50,7 +58,48 @@ export default {
             this.burgers = data;
             console.log(this.burgers);
 
-            // Status
+            // Request Status
+            this.getStatus();
+
+        },
+        async getStatus(){
+            const req = await fetch("http://localhost:3000/status");
+            const data = await  req.json();
+            this.status = data;
+            console.log(data);
+        },
+        async deleteBurger(id){
+            const req = await fetch(`http://localhost:3000/burgers/${id}`,{
+                method: "DELETE"
+            });
+            const res = await req.json();
+
+            this.msg = `Pedido removido com Sucesso`;
+
+            // apos 3min depois de enviar o pedido a msg apaga
+            setTimeout(() => this.msg = "", 3000);
+
+            this.getPedidos();
+        },
+        async updateBurger(event,id){
+            const option = event.target.value;
+
+            const dataJson = JSON.stringify({ status: option });
+
+            const req = await fetch(`http://localhost:3000/burgers/${id}`,{
+                method: "PATCH",
+                headers: {"Content-Type": "application/json"},
+                body: dataJson
+            });
+            const res = await req.json();
+
+            this.msg = `Pedido N.${res.id} foi atualizado para ${res.status}`;
+
+            // apos 3min depois de enviar o pedido a msg apaga
+            setTimeout(() => this.msg = "", 3000);
+
+            console.log(res);
+
         }
     },
     mounted(){
@@ -72,8 +121,10 @@ export default {
     }
     #burger-table-heading{
         font-weight: bold;
-        padding: 12px;
+        padding: 5px;
         border-bottom: 3px solid #333;
+        margin: 0em;
+
     }
     #burger-table-heading div,
     .burger-table-row div {
@@ -81,13 +132,14 @@ export default {
     }
     .burger-table-row {
         width: 100%;
-        padding: 12px;
+        padding: 5px;
         border: 1px solid #ccc;
+    
     }
 
     #burger-table-heading .order-id,
     .burger-table-row .order-number{
-        width: 5%;
+        width: 1%;
     }
     select {
         padding: 12px 6px;
